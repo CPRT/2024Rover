e729d082ca6b7b74fa3bb4f36ba79792db0a263c
ConnorN <connor.needham2015@gmail.com>
2025-02-20 09:36:36 -0500

    fix: navigation tuning

--- src/navigation/launch/nav2.launch.py
+++ src/navigation/launch/nav2.launch.py
@@ -123,7 +123,7 @@ def generate_launch_description():
 
     cmd_vel_topic_cmd = DeclareLaunchArgument(
         "cmd_vel_topic",
-        default_value="/drive/cmd_vel",
+        default_value="/cmd_vel",
         description="cmd_vel topic (for remmaping)",
     )
 
